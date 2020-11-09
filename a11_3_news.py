d75974601d62b0818572b6b32eae8095efca79b2
andy90598 <1105137110@nkust.edu.tw>
2020-11-09 14:07:54 +0800

    第一版
    第一次上傳

---
+++ a11_3_news.py
@@ -0,0 +1,29 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+def news():
+    r=requests.get('https://news.google.com/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JYcG9MVlJYR2dKVVZ5Z0FQAQ?hl=zh-TW&gl=TW&ceid=TW%3Azh-Hant')
+    soup=BeautifulSoup(r.text,'html.parser')
+    data=soup.select('.VDXfz+h3')
+    data2=soup.select('H3 ~ .QmrVtf.RD0gLb.kybdz>.SVJrMe>a')
+    b={}
+
+    for i in range (len(data)):
+        # print(data2[i].text," ",data[i].text)
+        if b.get(data2[i].text)==None:
+            b[data2[i].text]=[]
+        b[data2[i].text].append(data[i].text)
+    # print(b)
+    str1=json.dumps(b,indent=4,ensure_ascii=False)
+    return str1
+    # f=open('./json/text.json','w',encoding='UTF-8')
+    # f.write(str1)
+    # f.close()
+
+
+
+# data=soup.select('.VDXfz +h3')
+# data2 = soup.select('.SVJrMe > a')
+# for i in range(len(data)):
+#     print(data2[i].text," ",data[i].text)
+#     print("")
---
+++ backend.py
@@ -0,0 +1,17 @@
+from flask import Flask,request
+from a11_3_news import news
+app = Flask(__name__) #這段是一定要打的
+
+@app.route('/') #這段是路徑
+def hello_world():
+    return 'hello'
+
+@app.route('/abc',methods=['POST','GET']) #增加POST GET
+def hello_world2():
+    if request.method=='POST':
+        return "POST"
+    else:
+        news()
+        return news()
+if __name__ == '__main__':
+    app.run(host='127.0.0.1',port=5000)
