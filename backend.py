fa2817a6187e0536ed31334b70b4e3252eb53501
andy90598 <1105137110@nkust.edu.tw>
2020-11-11 10:05:52 +0800

    Update backend.py

--- backend.py
+++ backend.py
@@ -7,8 +7,8 @@ def hello_world():
     if request.method=='POST':
         data = request.get_json()
         print(data)
-        print('replay token=',data['event'][0]['replyToken'])
-        print('text=',data['event'][0]['message']['text'])
+        print('replay token=',data['events'][0]['replyToken'])
+        print('text=',data['events'][0]['message']['text'])
         return "POST"
     else:
         return "GET"
