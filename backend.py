9c3b5d25d9a7b80d80cc26595e46d50b0c25f4cf
andy90598 <1105137110@nkust.edu.tw>
2020-11-11 16:15:47 +0800

    Update backend.py

--- backend.py
+++ backend.py
@@ -18,12 +18,15 @@ def hello_world():
         replyMSG=text
         newstitle=''
         media=''
+        ################################################################媒體
         for i in a['headNews'][0]:
             media=media+i+'\n'
+        ################################################################標題
         if text in a['headNews'][0].keys():
             for i in range(len(a['headNews'][0][text]['title'])) :
                 newstitle=newstitle+a['headNews'][0][text]['title'][i]+'\n'+a['headNews'][0][text]['link'][i]+'\n'+'------------------------'+'\n'
             replyMSG=newstitle
+        ################################################################猜拳
         fist = ['剪刀','石頭','布']
         if text in fist:
                 ai = random.randint(0,2)
