f5ed5e598d1487d4a8c07b244642762fc5b4c3ef
andy90598 <1105137110@nkust.edu.tw>
2020-11-12 15:54:50 +0800

    更新
    新增卡片  排程器

--- Module/replyMessage.py
+++ Module/replyMessage.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 
-def ReplayMessage(replyToken, replyMSG,text,sticker,packageID,media):
+def ReplayMessage(replyToken, replyMSG, text, sticker, packageID, media):
     url = 'https://api.line.me/v2/bot/message/reply'
     accessToken = 'zTG6hdHrhApoeawkkdWpvspMdPq2Sc7SSztnQvIZmRiEWfamI8hFdMoRrpSoChN/ME27bdbC2nsCtchvVVfaY+CS0Tj8RQDAcqlTIq7ujZ6uAnn7UnmqxT/0X5fK4vq0UQrg9tEsTPJNlAT+JvOy4QdB04t89/1O/w1cDnyilFU='
     headers = {
@@ -38,6 +38,132 @@ def ReplayMessage(replyToken, replyMSG,text,sticker,packageID,media):
                     "text": "輸入想找的媒體"
                 },
             ]
+        }
+    elif text == '卡片':
+        {
+            "type": "flex",
+            "altText": "this is a flex message",
+            "contents": {
+                "type": "bubble",
+                "header": {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                        {
+                            "type": "text",
+                            "text": "NEWS DIGEST",
+                            "weight": "bold",
+                            "size": "sm",
+                            "color": "#AAAAAA",
+                            "contents": []
+                        }
+                    ]
+                },
+                "hero": {
+                    "type": "image",
+                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_4_news.png",
+                    "size": "full",
+                    "aspectRatio": "20:13",
+                    "aspectMode": "cover",
+                    "action": {
+                        "type": "uri",
+                        "label": "Action",
+                        "uri": "https://linecorp.com/"
+                    }
+                },
+                "body": {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "spacing": "md",
+                    "contents": [
+                        {
+                            "type": "box",
+                            "layout": "vertical",
+                            "flex": 1,
+                            "contents": [
+                                {
+                                    "type": "image",
+                                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/02_1_news_thumbnail_1.png",
+                                    "gravity": "bottom",
+                                    "size": "sm",
+                                    "aspectRatio": "4:3",
+                                    "aspectMode": "cover"
+                                },
+                                {
+                                    "type": "image",
+                                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/02_1_news_thumbnail_2.png",
+                                    "margin": "md",
+                                    "size": "sm",
+                                    "aspectRatio": "4:3",
+                                    "aspectMode": "cover"
+                                }
+                            ]
+                        },
+                        {
+                            "type": "box",
+                            "layout": "vertical",
+                            "flex": 2,
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "7 Things to Know for Today",
+                                    "size": "xs",
+                                    "flex": 1,
+                                    "gravity": "top",
+                                    "contents": []
+                                },
+                                {
+                                    "type": "separator"
+                                },
+                                {
+                                    "type": "text",
+                                    "text": "Hay fever goes wild",
+                                    "size": "xs",
+                                    "flex": 2,
+                                    "gravity": "center",
+                                    "contents": []
+                                },
+                                {
+                                    "type": "separator"
+                                },
+                                {
+                                    "type": "text",
+                                    "text": "LINE Pay Begins Barcode Payment Service",
+                                    "size": "xs",
+                                    "flex": 2,
+                                    "gravity": "center",
+                                    "contents": []
+                                },
+                                {
+                                    "type": "separator"
+                                },
+                                {
+                                    "type": "text",
+                                    "text": "LINE Adds LINE Wallet",
+                                    "size": "xs",
+                                    "flex": 1,
+                                    "gravity": "bottom",
+                                    "contents": []
+                                }
+                            ]
+                        }
+                    ]
+                },
+                "footer": {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                        {
+                            "type": "button",
+                            "action": {
+                                "type": "uri",
+                                "label": "More",
+                                "uri": "https://linecorp.com"
+                            }
+                        }
+                    ]
+                }
+            }
         }
     else:
         data = {
