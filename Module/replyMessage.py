c3a96270f988c8c94c10291922bd1d2afe46a43a
andy90598 <1105137110@nkust.edu.tw>
2020-11-12 16:10:37 +0800

    Update replyMessage.py

--- Module/replyMessage.py
+++ Module/replyMessage.py
@@ -1,7 +1,6 @@
 import requests
 import json
 def ReplayMessage(replyToken, replyMSG, text, sticker, packageID, media):
-    global data
     url = 'https://api.line.me/v2/bot/message/reply'
     accessToken = 'zTG6hdHrhApoeawkkdWpvspMdPq2Sc7SSztnQvIZmRiEWfamI8hFdMoRrpSoChN/ME27bdbC2nsCtchvVVfaY+CS0Tj8RQDAcqlTIq7ujZ6uAnn7UnmqxT/0X5fK4vq0UQrg9tEsTPJNlAT+JvOy4QdB04t89/1O/w1cDnyilFU='
     headers = {
